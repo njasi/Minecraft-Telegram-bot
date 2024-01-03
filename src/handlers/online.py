e856e4884732b517f3a1bd6970885e65d5aaec5f
njasi <nick@jasinski3.com>
2024-01-02 16:53:27 -0800

    no players online message

--- src/handlers/online.py
+++ src/handlers/online.py
@@ -1,15 +1,18 @@
 import os
 from telegram import Update
 from telegram.ext import ContextTypes
-from render.online import render_online
+from render.online import render_online, N
 
 
 async def online(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Displays server status, and motd render"""
 
-    photo = (
-        render_online(addr=context.args[0]) if len(context.args) == 1 else render_online()
-    )
+    try:
+        photo = (
+            render_online(addr=context.args[0]) if len(context.args) == 1 else render_online()
+        )
+        await update.effective_message.reply_photo(photo=photo)
+    except:
+        await update.effective_message.reply_html("There are no players online.")
 
-    await update.effective_message.reply_photo(photo=photo)
     os.remove(photo)
